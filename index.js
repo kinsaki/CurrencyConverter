fbdcb323b092dda1bd9762d1d6333c9f97ad07b7
devonlucey <31045924+devonlucey@users.noreply.github.com>
2020-02-18 16:25:45 -0600

    First Review
    Add eslint
    Remove body-parser
    Better variables
    Remove start and just accept a post

--- index.js
+++ index.js
@@ -1,47 +1,40 @@
-const express = require('express')
-const https = require('https')
-const bodyParser = require('body-parser')
+const express = require('express');
+const https = require('https');
 
-const app = express()
-const port = 3000
-app.listen(port, () => console.log(`Successfully started currency converter!`))
-app.use(bodyParser.urlencoded({ extended: true }))
-
-const start = () => {
-	return new Promise(resolve => {
-		acceptPost()
-	})
-}
+const app = express();
+const port = 3000;
+app.use(express.urlencoded({ extended: false }));
 
 const acceptPost = () => {
-	app.post('/', function(request, response, next) {
-	currencyFrom = request.body['currencyFrom']
-	currencyFromValue = request.body['currencyFromValue']
-	currencyTo = request.body['currencyTo']
+	app.post('/', function(request, response) {
+		const currencyFrom = request.body['currencyFrom'];
+		const currencyFromValue = request.body['currencyFromValue'];
+		const currencyTo = request.body['currencyTo'];
+		getCurrencies(currencyFrom, currencyFromValue, currencyTo);
 
-	response.writeHead(200, {'Content-Type': 'text/html'})
-	response.end()
-	getCurrencies()
-})
-}
+		response.writeHead(200, {'Content-Type': 'text/html'});
+		response.end();
+	});
+};
 
-const getCurrencies = () => {
+const getCurrencies = (currencyFrom, currencyFromValue, currencyTo) => {
 	https.get(`https://api.exchangerate-api.com/v4/latest/${currencyFrom}`, (res) => {
 
 	res.on('data', (data) => {
-	parsedRates = (JSON.parse(data)['rates'])
-	currencyToValue = parsedRates[currencyTo]
-	convertCurrencies()
-	})
+		const parsedRates = (JSON.parse(data)['rates']);
+		const currencyToValue = parsedRates[currencyTo];
+		convertCurrencies(currencyFromValue, currencyToValue, currencyTo);
+	});
 
-	}).on("error", (err) => {
-	  console.log("Error: " + err.message)
-	})
-}
+	}).on('error', (err) => {
+		console.log('Error: ' + err.message);
+	});
+};
 
-const convertCurrencies = () => {
-	convertedValue = currencyFromValue * currencyToValue
-	console.log(convertedValue.toFixed(2), currencyTo)
-}
+const convertCurrencies = (currencyFromValue, currencyToValue, currencyTo) => {
+	const convertedValue = currencyFromValue * currencyToValue;
+	console.log(convertedValue.toFixed(2), currencyTo);
+};
 
-start()
+app.listen(port, () => console.log('Successfully started currency converter!'));
+acceptPost();
